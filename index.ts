a0a5b2b3d0439523820df842193d7831bf28e166
takanakahiko <takanakahiko@gmail.com>
2021-08-11 00:32:25 +0900

    migrate index script to typescript

--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
-//これ何を宣言している？
-const http = require('http');
-const querystring = require('querystring');
-const { Client, Intents } = require('discord.js');
+// パッケージのインポート
+
+import { Client, Intents, TextBasedChannels, Message } from 'discord.js'
+
 require('dotenv').config();
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES] });
 
 //Bot側で返すテキスト
 let RepText = "";
 
 //ターミナル上の起動確認
-client.on('ready', message =>{
+client.on('ready', _ =>{
   console.log('Bot準備完了～');
   //起動確認用 sendMsg(message.channels.id, "起きましたぁ");
-  //Ver古いので確認する
-  client.user.setPresence({ activity: { name: 'げーむ' } });
+  if(client.user == null){
+    console.log("error: client.user が null でした");
+    return
+  }
+  client.user.setPresence({ activities: [{ name: 'げーむ' }] });
 });
 
 //指定ワードに対する返答
 client.on('messageCreate', message =>{
+
+  if(client.user == null){
+    console.log("error: client.user が null でした");
+    return
+  }
+
   //取得したワードがBotからか確認
   if (message.author.id == client.user.id){
     return;
@@ -25,6 +35,7 @@ client.on('messageCreate', message =>{
     //取得したワードをターミナル上に表示
     console.log("メッセージ受信: " + message.content);
   }
+
   //Botにリアクションがあった場合
   if(message.mentions.has(client.user)){
     RepText = "呼んだ？";
@@ -33,9 +44,10 @@ client.on('messageCreate', message =>{
   }
   //対象メッセージに対するリアクション
   if (message.content.match(/にゃ～ん|にゃーん/)){
-    sendMsg(message.channel.id, "にゃ～ん");
+    sendMsg(message.channel, "にゃ～ん");
     return;
   }
+
   if (message.content.match(/おみくじ/)){
     const min = 1;
     const max = 5;
@@ -51,7 +63,7 @@ client.on('messageCreate', message =>{
     console.log("ランダムNo. " + randomNumber);
     console.log(RepText);
     //おみくじ結果を返信
-    sendMsg(message.channel.id, RepText);
+    sendMsg(message.channel, RepText);
     return;
   }
 });
@@ -60,15 +72,13 @@ client.on('messageCreate', message =>{
 client.login( process.env.DISCORD_BOT_TOKEN );
 
 //Bot側のリプライをターミナル上で表示
-function sendReply(message, text){
+function sendReply(message: Message, text: string){
   message.reply(text)
-    .then(console.log("リプライ送信: " + text))
+    .then(() => console.log("リプライ送信: " + text))
     .catch(console.error);
 }
 
 //この処理何？
-function sendMsg(channelId, text, option={}){
-  client.channels.fetch(channelId)
-    .then(channel => channel.send(text, option))
-    .catch(console.error);
+function sendMsg(channel: TextBasedChannels , text: string){
+  channel.send(text)
 }
