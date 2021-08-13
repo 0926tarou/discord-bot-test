c64caf158107f6e29e9345926b37bb81f619c1dd
0926tarou <road.to.victory.good@gmail.com>
2021-08-13 21:12:46 +0900

    音声取得テスト

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 // パッケージのインポート
-
-import { Client, Intents, TextBasedChannels, Message } from 'discord.js'
-
-require('dotenv').config();
-
+import { Client, Intents, TextBasedChannels, Message, } from 'discord.js'
+//この処理何？
+require('dotenv').config();;
+//この処理何？
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES] });
 
 //ターミナル上の起動確認
@@ -15,9 +14,61 @@ client.on('ready', _ =>{
   }
   client.user.setPresence({ activities: [{ name: 'げーむ' }] });
 });
+/*
+//テストhttps://qiita.com/atticatticattic/items/c9e7d5248fda563d68b5
+// make a new stream for each time someone starts to talk
+function generateOutputFile(channel:TextBasedChannels, member) {
+  // use IDs instead of username cause some people have stupid emojis in their name
+  const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+  return fs.createWriteStream(fileName);
+}
+//テストここまで*/
 
-//指定ワードに対する返答
+//指定ワードに対する対応
 client.on('messageCreate', message =>{
+  //音声取得テスト
+  /*if (message.content.startsWith(config.prefix+'join')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    if (!msg.guild) {
+        return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
+    }
+    const voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    //console.log(voiceChannel.id);
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+        return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    voiceChannel.join()
+      .then(conn => {
+        msg.reply('ready!');
+        // create our voice receiver
+        const receiver = conn.createReceiver();
+
+        conn.on('speaking', (user, speaking) => {
+          if (speaking) {
+            msg.channel.sendMessage(`I'm listening to ${user}`);
+            // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
+            const audioStream = receiver.createPCMStream(user);
+            // create an output stream so we can dump our data in a file
+            const outputStream = generateOutputFile(voiceChannel, user);
+            // pipe our audio data into the file stream
+            audioStream.pipe(outputStream);
+            outputStream.on("data", console.log);
+            // when the stream ends (the user stopped talking) tell the user
+            audioStream.on('end', () => {
+                msg.channel.sendMessage(`I'm no longer listening to ${user}`);
+            });
+          }
+        });
+      })
+    .catch(console.log);
+  }
+  if(msg.content.startsWith(config.prefix+'leave')) {
+    let [command, ...channelName] = msg.content.split(" ");
+    let voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
+    voiceChannel.leave();
+  }
+  //テストここまで*/
+
   //Bot側で返すテキスト
   let RepText = "";
 
